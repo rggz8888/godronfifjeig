dfabee3f9b68dff97a663b039c04a09e15e662ac
yzhaorong <yzhaorong@gmail.com>
2013-11-15 01:44:37 +0800

    将pic_list中brief字段不要去除标签。

--- plugin/xmulife/post_thread_succeed.php
+++ plugin/xmulife/post_thread_succeed.php
@@ -1,4 +1,30 @@
+//合并pic_list插件中update_succeed文件
+
+$fid = $thread['fid'];
+$tid = $thread['tid'];
+$pid = $thread['firstpid'];
+$thread['coverimg'] = '';
+if($thread['imagenum'] > 0) {
+	$attachlist = $this->attach->index_fetch(array('fid'=>$fid, 'pid'=>$pid), array(), 0, 20);
+	foreach($attachlist as $k=>$attach) {
+		if($attach['isimage'] == 1) {
+			$thread['coverimg'] = $attach['filename'];
+			break;
+		}
+	}
+}
+
+$firstpost = $this->post->read($thread['fid'], $thread['firstpid']);
+$message = $firstpost['message'];
+//$message = htmlspecialchars(strip_tags($firstpost['message'],'<br> <p> <i> <b> <span> <a>'));
+//$message = str_replace(array('&nbsp;', '&amp;', '&quot;', '&#039;', ' &lt;', '&gt;'), array(' ', '&', '\"', '\'', '<','>'), $message);
+//$message = $firstpost['message'];
+//$thread['brief'] = utf8::cutstr_cn($message, 200);
+$thread['brief'] = utf8::cutstr_cn($message, 1200);
+$this->thread->update($thread);
+
 //以下同步至新浪微博代码
+
 $post2weibo = core::gpc('post2weibo','P');
 if($post2weibo){
 function postData($url, $data){  
