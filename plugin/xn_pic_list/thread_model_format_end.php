2d5a05c2868e0900df0ee27707d15e5e62388eb3
yzhaorong <yzhaorong@gmail.com>
2013-11-28 18:50:26 +0800

    修改列表td宽度

--- plugin/xn_pic_list/thread_model_format_end.php
+++ plugin/xn_pic_list/thread_model_format_end.php
@@ -3,4 +3,6 @@ if(empty($_SERVER['_baiduBCS'])) {
 			$_SERVER['_kc_bcs_conf'] = $this->kv->get('kc_bcs_conf');
 			$_SERVER['_baiduBCS'] = new BaiduBCS ($_SERVER['_kc_bcs_conf']['ak'], $_SERVER['_kc_bcs_conf']['sk'], 'bcs.duapp.com');
 		}
+if(substr($thread['coverimg'],0,4) != 'http')
 $thread['coverimg'] && $thread['coverimg'] = $_SERVER['_kc_bcs_conf']['remotepath'].'/attach/'.$thread['coverimg'];
+else $thread['coverimg'] && $thread['coverimg'] = $thread['coverimg'];
